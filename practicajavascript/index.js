04508f8744eec83970099627cf4c0029350c02d0
ValentinAndres <valentinandress@gmail.com>
2024-04-21 16:18:47 -0400

    cambio

---
+++ practicajavascript/index.js
@@ -0,0 +1,10 @@
+function sumar(){
+
+    var v1=parseInt(document.getElementById("valor1").value);
+    var v2=parseInt(document.getElementById("valor1").value);
+    var suma=0;
+
+    suma =v1+v2;
+
+    document.getElementById("resultado").value=suma;
+} 
